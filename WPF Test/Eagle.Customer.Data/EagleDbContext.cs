0a84da58bc319f849376a43817743fa444402c43
Juan Rosario <jrosario@eaglemetal.com>
2025-04-17 08:22:19 -0500

    Start building test projects

---
+++ WPF Test/Eagle.Customer.Data/EagleDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Eagle.CustomerApp.Model;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+namespace Eagle.CustomerApp.Data
+{
+    public class EagleDbContext : DbContext
+    {
+        DbSet<Customer> Customers { get; set; }
+
+        public EagleDbContext()
+        {
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Console.WriteLine(Path.Combine(GetDatabaseDirectory(), "EagleTestDb.db"));
+            SqliteConnectionStringBuilder connectionString = new()
+            {
+                DataSource = Path.Combine(GetDatabaseDirectory(), "EagleTestDb.db"),
+                Mode = SqliteOpenMode.ReadWriteCreate,
+            };
+            Console.WriteLine(connectionString);
+            optionsBuilder.UseSqlite(connectionString.ToString());
+        }
+
+        /// <summary>
+        /// Implemented this to make sure we find the proper database file path for test purposes
+        /// </summary>
+        /// <returns>Path where Database directory exists</returns>
+        private string GetDatabaseDirectory()
+        {
+            var directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var wpfTextIndex = directory.LastIndexOf("WPF Test\\");
+            return Path.Combine(directory.Substring(0, wpfTextIndex + 8), "Database");
+
+        }
+
+    }
+}
---
+++ WPF Test/Eagle.Customer.Model/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Eagle.CustomerApp.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; } 
+        public required string Name { get; set; }
+
+
+    }
+}
